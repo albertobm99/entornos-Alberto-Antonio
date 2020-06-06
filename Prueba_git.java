5f03b2a41350790c0c90db9d2026bbcecfbd1dc5
albertobm99 <albertorr@alumnos.iesgalileo.es>
2020-06-06 12:45:52 +0200

    primera version sin switch

---
+++ Prueba_git.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_git;
+
+/**
+ *
+ * @author Usuario1
+ */
+public class Prueba_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        String mes=teclado.nextLine();
+    }
+    
+}
