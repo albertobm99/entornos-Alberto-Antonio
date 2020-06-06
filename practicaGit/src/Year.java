c633b0109504423db3204c6a093d4a3dcf0b72dc
antonioentornos <lacisternigabasket@gmail.com>
2020-06-06 19:01:51 +0200

    Proyecto con Ventana y Year

---
+++ practicaGit/src/Year.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sabaa
+ */
+
+import java.util.Arrays;
+public class Year {
+private static String[]
+meses={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
+private static int[] dias={ 31,28,31,30,31,30,31,31,30,31,30,31};
+public static int getDiasMes(String nombre){
+int indice=buscar(nombre);
+return dias[indice];
+}
+public static int getDiasMes(int nMes){
+int indice=nMes-1;
+return dias[indice];
+}
+public static String getNombreMes(int nMes){
+int indice=nMes-1;
+return meses[indice];
+}
+private static int buscar(String mesBuscado){
+for(int indice=0;indice<12;indice++){
+if(mesBuscado.equals(meses[indice])){
+return indice;
+}
+}
+return -1;
+}
+}
